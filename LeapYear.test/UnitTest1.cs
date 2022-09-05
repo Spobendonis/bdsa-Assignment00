b2f80a46ce54a5ff18e78cc4edac131c815417ee
Tobias Gad Spoorendonk <tosp@itu.dk>
2022-09-05 12:10:50 +0200

    added 100 year rule test and implementation

--- LeapYear.test/UnitTest1.cs
+++ LeapYear.test/UnitTest1.cs
@@ -19,4 +19,15 @@ public class UnitTest1
         Assert.False(LeapYear.isLeapYear(499));
         Assert.False(LeapYear.isLeapYear(8010));
     }
+
+        [Fact]
+    public void Every_100_Years_Not_Leap_Year()
+    {
+        Assert.False(LeapYear.isLeapYear(100));
+        Assert.False(LeapYear.isLeapYear(200));
+        Assert.False(LeapYear.isLeapYear(600));
+        Assert.False(LeapYear.isLeapYear(1700));
+        Assert.False(LeapYear.isLeapYear(1900));
+        Assert.False(LeapYear.isLeapYear(2100));
+    }
 }
