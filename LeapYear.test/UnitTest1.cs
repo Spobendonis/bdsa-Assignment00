e1ef480533900cd456c47d18c3c8fc5b57eaac87
Tobias Gad Spoorendonk <tosp@itu.dk>
2022-09-05 11:20:23 +0200

    set up environment and created first test

---
+++ LeapYear.test/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Xunit;
+
+namespace LeapYear.test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Every_4_Years_Is_Leap_Year()
+    {
+        Assert.True(LeapYear.isLeapYear(4));
+        Assert.True(LeapYear.isLeapYear(8));
+        Assert.True(LeapYear.isLeapYear(416));
+        Assert.True(LeapYear.isLeapYear(444));
+        Assert.True(LeapYear.isLeapYear(4804));
+        Assert.True(LeapYear.isLeapYear(4824));
+        Assert.True(LeapYear.isLeapYear(5220));
+        Assert.False(LeapYear.isLeapYear(7));
+        Assert.False(LeapYear.isLeapYear(402));
+        Assert.False(LeapYear.isLeapYear(500));
+        Assert.False(LeapYear.isLeapYear(8010));
+    }
+}
---
+++ LeapYear/LeapYear.cs
@@ -0,0 +1,10 @@
+﻿namespace LeapYear;
+
+public class LeapYear {
+    static void Main(string[] args) {
+
+    }
+    public static bool isLeapYear(int year) {
+        throw new NotImplementedException();
+    }
+}
