c2481c6193148dcbe8f7ad27b3c96d91290ccba5
Tobias Gad Spoorendonk <tosp@itu.dk>
2022-09-06 12:56:16 +0200

    added user input, exception handling and exception handling tests

--- LeapYear.test/UnitTest1.cs
+++ LeapYear.test/UnitTest1.cs
@@ -1,5 +1,3 @@
-using Xunit;
-
 namespace LeapYear.test;
 
 public class UnitTest1
@@ -7,39 +5,58 @@ public class UnitTest1
     [Fact]
     public void Every_4_Years_Is_Leap_Year()
     {
-        Assert.True(LeapYear.isLeapYear(4));
-        Assert.True(LeapYear.isLeapYear(8));
-        Assert.True(LeapYear.isLeapYear(416));
-        Assert.True(LeapYear.isLeapYear(444));
-        Assert.True(LeapYear.isLeapYear(4804));
-        Assert.True(LeapYear.isLeapYear(4824));
-        Assert.True(LeapYear.isLeapYear(5220));
-        Assert.False(LeapYear.isLeapYear(7));
-        Assert.False(LeapYear.isLeapYear(402));
-        Assert.False(LeapYear.isLeapYear(499));
-        Assert.False(LeapYear.isLeapYear(8010));
+        Assert.True(LeapYear.isLeapYear("2004"));
+        Assert.True(LeapYear.isLeapYear("2140"));
+        Assert.True(LeapYear.isLeapYear("4804"));
+        Assert.True(LeapYear.isLeapYear("4824"));
+        Assert.True(LeapYear.isLeapYear("5220"));
+        Assert.False(LeapYear.isLeapYear("8010"));
+        Assert.False(LeapYear.isLeapYear("8021"));
+        Assert.False(LeapYear.isLeapYear("9777"));
     }
 
     [Fact]
     public void Every_100_Years_Not_Leap_Year()
     {
-        Assert.False(LeapYear.isLeapYear(100));
-        Assert.False(LeapYear.isLeapYear(200));
-        Assert.False(LeapYear.isLeapYear(600));
-        Assert.False(LeapYear.isLeapYear(1700));
-        Assert.False(LeapYear.isLeapYear(1900));
-        Assert.False(LeapYear.isLeapYear(2100));
+        Assert.False(LeapYear.isLeapYear("1700"));
+        Assert.False(LeapYear.isLeapYear("1900"));
+        Assert.False(LeapYear.isLeapYear("2100"));
+        Assert.False(LeapYear.isLeapYear("2200"));
+        Assert.False(LeapYear.isLeapYear("2300"));
+        Assert.False(LeapYear.isLeapYear("2500"));
     }
 
     [Fact]
     public void Every_400_Years_Is_Leap_Year()
     {
-        Assert.True(LeapYear.isLeapYear(0));
-        Assert.True(LeapYear.isLeapYear(400));
-        Assert.True(LeapYear.isLeapYear(800));
-        Assert.True(LeapYear.isLeapYear(1200));
-        Assert.True(LeapYear.isLeapYear(1600));
-        Assert.True(LeapYear.isLeapYear(2000));
-        Assert.True(LeapYear.isLeapYear(2400));
+        Assert.True(LeapYear.isLeapYear("1600"));
+        Assert.True(LeapYear.isLeapYear("2000"));
+        Assert.True(LeapYear.isLeapYear("2400"));
+        Assert.True(LeapYear.isLeapYear("10000"));
+        Assert.True(LeapYear.isLeapYear("12000"));
+        Assert.True(LeapYear.isLeapYear("40000"));
+        Assert.True(LeapYear.isLeapYear("80000"));
+    }
+
+    [Fact]
+    private void Number_Too_Low_Fails()
+    {
+        Assert.False(LeapYear.isLeapYear("145"));
+        Assert.False(LeapYear.isLeapYear("199"));
+        Assert.False(LeapYear.isLeapYear("200"));
+        Assert.False(LeapYear.isLeapYear("204"));
+        Assert.False(LeapYear.isLeapYear("400"));
+        Assert.False(LeapYear.isLeapYear("908"));
+        Assert.False(LeapYear.isLeapYear("1520"));
+    }
+    private void Not_A_Number_Fails()
+    {
+        Assert.False(LeapYear.isLeapYear("Hello"));
+        Assert.False(LeapYear.isLeapYear("19 04"));
+        Assert.False(LeapYear.isLeapYear("1800x"));
+        Assert.False(LeapYear.isLeapYear("rx20000mb"));
+        Assert.False(LeapYear.isLeapYear("5000pp"));
+        Assert.False(LeapYear.isLeapYear("50 80 pp"));
+        Assert.False(LeapYear.isLeapYear("pp 50 80 20"));
     }
 }
--- LeapYear/LeapYear.cs
+++ LeapYear/LeapYear.cs
@@ -2,10 +2,26 @@
 
 public class LeapYear {
     static void Main(string[] args) {
-
+        string input;
+        Console.WriteLine("Welcome, I the undisputed master of the Gregorian Calendar will now answer your question about any date; leap year, yay or nay? Please say a year from 1582 or after!");
+        input = Console.ReadLine();
+        isLeapYear(input);
     }
-    public static bool isLeapYear(int year) {
+    public static bool isLeapYear(string inputYear) {
         var leapYear = false;
+        int year;
+        try {
+            year = Int32.Parse(inputYear);
+            if(year < 1582) {
+                throw new InvalidDataException();
+            }
+        } catch (InvalidDataException) {
+            Console.WriteLine("This number is too small, It must be greater than 1581!");
+            return false;
+        } catch (Exception) {
+            Console.WriteLine("This is not a number!");
+            return false;
+        }
         if(year % 4 == 0) {
             leapYear = !leapYear;
         }
@@ -14,6 +30,11 @@ public class LeapYear {
         }
         if(year % 400 == 0) {
             leapYear = !leapYear;
+        }
+        if(leapYear) {
+            Console.WriteLine("yay");
+        } else {
+            Console.WriteLine("nay");
         }
         return leapYear;
     }
